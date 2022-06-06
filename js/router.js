237b4862bef7fa42dc5fb6d93438d2d00d6ddc0b
Carolina Dutra <carolinandutra@hotmail.com>
2022-06-06 17:25:48 -0300

    created repository

---
+++ js/router.js
@@ -0,0 +1,49 @@
+export class Router {
+  
+  routes = {}
+  
+  add(routeName, page){
+    this.routes[routeName] = page
+  }
+
+  route(event){
+    event = event || window.event
+    event.preventDefault()
+
+    window.history.pushState({}, "", event.target.href)
+    this.handle()
+  }
+
+  handle(){
+    const {pathname} = window.location
+    const route = this.routes[pathname]
+    this.changeBg()
+
+    fetch(route).then(data => data.text()).then(html =>{
+      document.querySelector("#app").innerHTML = html
+    })
+  }
+
+  changeBg(){
+    const {pathname} = window.location
+    const {body} = document
+
+    switch(pathname){
+      case '/':
+        body.className = 'home'
+        break
+
+      case '/theUniverse':
+        body.className = 'universe'
+        break
+
+      case '/exploration':
+        body.className = 'exploration'
+        break
+      
+      default:
+        body.className = ''
+        break
+    }
+  }
+}
